5ffc13723e1e0e216ac0ab45780852ba817897ed
gerard.romo <gerard.romo.7e9@itb.cat>
2025-12-17 12:27:20 +0100

    ACTU

--- client/src/components/examples/Header.tsx
+++
@@ -1,5 +0,0 @@
-import Header from "../Header";
-
-export default function HeaderExample() {
-  return <Header onNavigate={(href) => console.log("Navigate to:", href)} />;
-}
--- client/src/components/examples/NeonLogo.tsx
+++
@@ -1,5 +0,0 @@
-import NeonLogo from "../NeonLogo";
-
-export default function NeonLogoExample() {
-  return <NeonLogo initials="GR" />;
-}
